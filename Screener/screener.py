fb466589c35da428178df46e2a46e91e55bec53e
Yahaswa Varshney <yswa.var@gmail.com>
2023-09-26 18:56:01 +0530

    sk

--- Screener/screener.py
+++ Screener/screener.py
@@ -2,28 +2,20 @@ import requests
 from bs4 import BeautifulSoup as bs
 import pandas as pd
 
-url = "https://www.screener.in/company/OFSS/"
-response = requests.get(url)
+def get_shareholding(x):
+    url = "https://www.screener.in/company/" + x +"/"
+    response = requests.get(url)
 
-soup = bs(response.content, "html.parser")
+    soup = bs(response.content, "html.parser")
 
+    quarterly_shp_section = soup.find('div', {'id': 'quarterly-shp'})
 
-# Find the section with id "quarterly-shp"
-quarterly_shp_section = soup.find('div', {'id': 'quarterly-shp'})
+    shareholding_table = quarterly_shp_section.find('table', {'class': 'data-table'})
+    shareholding_data = []
+    for row in shareholding_table.find_all('tr'):
+        cols = row.find_all('td')
+        cols = [ele.text.strip() for ele in cols]
+        shareholding_data.append(cols)
 
-# Find the shareholding pattern table within the section
-shareholding_table = quarterly_shp_section.find('table', {'class': 'data-table'})
-
-# Extract table data into a list of lists
-shareholding_data = []
-for row in shareholding_table.find_all('tr'):
-    cols = row.find_all('td')
-    cols = [ele.text.strip() for ele in cols]
-    shareholding_data.append(cols)
-
-# # Print the shareholding data
-# for row in shareholding_data:
-#     print(row)
-
-df = pd.DataFrame(shareholding_data)
-print(df)
+    df = pd.DataFrame(shareholding_data)
+    return df
